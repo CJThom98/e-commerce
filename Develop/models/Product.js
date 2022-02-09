beb4671e2abfed8dad309f87f2428dbd3aab6efa
Cory Thompson <cory.thompson98@gmail.com>
2022-02-09 14:09:15 -0600

    models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +10,44 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // price column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        value = DECIMAL
+      }
+    },
+    // stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      value: 10,
+      validate: {
+
+      }
+    },
+    // category id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
